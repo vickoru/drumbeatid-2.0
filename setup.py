46773e2ed52e9489d68078a43ea77cf240e0af96
Victor Ruiz <vicruiz85@gmail.com>
2023-08-06 18:42:10 +0200

    Added requirements, setup and env files

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open('requirements.txt') as f:
 requirements = [x.strip() for x in content if 'git+' not in x]
 
 setup(name='drumbeatid-2.0',
-      version="1.0",
-      description="Project Description",
+      version="2.0",
+      description="Version 2.0 of the project drumbeatID",
       packages=find_packages(),
       install_requires=requirements,
       test_suite='tests',
