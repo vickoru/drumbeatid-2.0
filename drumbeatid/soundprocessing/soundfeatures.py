f9e1c4917316cfd1023d423e8977df1591cd15ad
Victor Ruiz <vicruiz85@gmail.com>
2023-08-07 16:38:16 +0200

    Last version of the new soundfeatures.py, implemented sound features

--- drumbeatid/soundprocessing/soundfeatures.py
+++ drumbeatid/soundprocessing/soundfeatures.py
@@ -2,20 +2,36 @@ import numpy as np
 import librosa
 import os
 
-from drumbeatid.ml_logic.preprocessor import Audio
 
 class Audio:
     '''
     Librosa features coded in a class
     '''
     def __init__(self, audiofile, duration=6):
-        ## Get the waveforms and the sampling rate from class Audio
+        '''
+        Initialize class by getting the he waveforms and the sampling rate
+        from librosa
+        '''
         self.waveform, self.samplingrate = librosa.load(
             audiofile, duration=duration)
 
+    def calculate_spectrogram(self, nfft=2048, hoplength=512):
+        '''
+        ===  1 Spectrogram  ===
+        requires 1-dimensional array x(timeseries), sampling rate sr
+        returns spectrogram, a matplotlib.colormesh object
+
+        '''
+        #performing short time- Fourier analysis
+        X = librosa.stft(self.waveform, n_fft=nfft, hop_length=hoplength)
+
+        #amplitudes of given frequency at given time -> spectrogram
+        self.Xdb = librosa.amplitude_to_db(abs(X))
+
+
     def calculate_mfccs(self, number_coeffs=128):
         '''
-        Caluculate Mel frequency cepstral coefficients.
+        Calculate Mel frequency cepstral coefficients.
         Input:
         waveform: array representing the waveform originating from audio file
         samplingrate: sampling rate from audio
@@ -26,17 +42,21 @@ class Audio:
                                  n_mfcc=number_coeffs)
 
 
-    def calculate_spectrogram(self, nfft=2048, hoplength=512):
+    def calculate_chroma(self, nfft=2048, hoplength=512):
         '''
-        ===  1 Spectogram  ===
-        requires 1-dimensional array x(timeseries), sampling rate sr
-        returns spectogram, a matplotlib.colormesh object
+        Chroma Feature calculation with a hop length of 512 as default
 
         '''
-        #performing short time- Fourier analysis
-        X = librosa.stft(self.waveform, n_fft=nfft, hop_length=hoplength)
+        self.chromafeat = librosa.feature.chroma_stft(
+            y=self.waveform, sr=self.samplingrate,
+            n_fft=nfft, hop_length=hoplength)
 
-        #amplitudes of given frequency at given time -> spectrogram
-        self.Xdb = librosa.amplitude_to_db(abs(X))
+    def calculate_melspect(self, nfft=2048, hoplength=512):
+        '''
+        Compute a mel-scaled spectrogram
+        '''
+        melspec = librosa.feature.melspectrogram(
+            y=self.waveform, sr=self.samplingrate,
+            n_fft=nfft, hop_length=hoplength)
 
-        return self.Xdb
+        self.melspec_dB = librosa.power_to_db(melspec)
