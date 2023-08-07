cbe38668c60c8b76e64ca23d0df97def5abb30a1
Victor Ruiz <vicruiz85@gmail.com>
2023-08-07 20:08:10 +0200

    Done with the Audio class for sound processing, coding CustomMinMaxScaler, closing this branch and opening new one

--- drumbeatid/soundprocessing/soundfeatures.py
+++ drumbeatid/soundprocessing/soundfeatures.py
@@ -7,15 +7,26 @@ class Audio:
     '''
     Librosa features coded in a class
     '''
-    def __init__(self, audiofile, duration=6):
+    def __init__(self, audiofile, sr=22050, duration=6):
         '''
         Initialize class by getting the he waveforms and the sampling rate
         from librosa
+        Default duration of the sample is 6 seconds, with a sampling rate of
+        22050 samples per secong, default in librosa.
         '''
-        self.waveform, self.samplingrate = librosa.load(
-            audiofile, duration=duration)
+        max_length = duration * sr
+        waveform_, samplingrate_ = librosa.load(
+            audiofile, sr=sr, duration=duration)
 
-    def calculate_spectrogram(self, nfft=2048, hoplength=512):
+        if waveform_.shape[0] < max_length:
+            waveform_ = librosa.util.pad_center(
+                waveform_, size=max_length, axis=0)
+
+        self.waveform = waveform_
+        self.samplingrate = samplingrate_
+
+
+    def calculate_spectrogramstft(self, nfft=2048, hoplength=512):
         '''
         ===  1 Spectrogram  ===
         requires 1-dimensional array x(timeseries), sampling rate sr
@@ -27,6 +38,8 @@ class Audio:
 
         #amplitudes of given frequency at given time -> spectrogram
         self.Xdb = librosa.amplitude_to_db(abs(X))
+
+        return self.Xdb
 
 
     def calculate_mfccs(self, number_coeffs=128):
@@ -41,6 +54,8 @@ class Audio:
         self.mfccs = librosa.feature.mfcc(y=self.waveform, sr=self.samplingrate,
                                  n_mfcc=number_coeffs)
 
+        return self.mfccs
+
 
     def calculate_chroma(self, nfft=2048, hoplength=512):
         '''
@@ -51,6 +66,9 @@ class Audio:
             y=self.waveform, sr=self.samplingrate,
             n_fft=nfft, hop_length=hoplength)
 
+        return self.chromafeat
+
+
     def calculate_melspect(self, nfft=2048, hoplength=512):
         '''
         Compute a mel-scaled spectrogram
@@ -60,3 +78,15 @@ class Audio:
             n_fft=nfft, hop_length=hoplength)
 
         self.melspec_dB = librosa.power_to_db(melspec)
+
+        return self.melspec_dB
+
+    def get_all_features(self):
+        '''
+        Calculate all features with the default settings
+        '''
+
+        self.Xdb = self.calculate_spectrogramstft()
+        self.mfccs = self.calculate_mfccs()
+        self.chromafeat = self.calculate_chroma()
+        self.melspec_dB = self.calculate_melspect()
